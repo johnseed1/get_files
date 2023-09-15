189ce3ce03e190f608e738124da79b0c62a277a3
johnseed1 <144424336+johnseed1@users.noreply.github.com>
2023-09-15 11:07:47 +0545

    Create get_files.py

---
+++ get_files.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+import shutil
+
+## Get the dataframe from the encrypted csv file
+def decrypt_df(encrypted_df, encryption_key):
+  pass
+
+def download_file(url):
+  pass
+
+def download_all_files(url):
+  pass
